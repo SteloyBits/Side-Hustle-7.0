cb83af9b56fcbadda6d2649f826ab4f42f04d391
steloybits <baruwaabdulsamad900@gmail.com>
2023-03-13 01:40:05 +0100

    new file containing assignment operation of name
    New file, index.php was created and a simple assignment operation was carried out in it

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+  //Here i declare a variable `name` and give it a string value containing my name
+  $name = "Abdulsamad";
+  //The line below outputs the value of the variable name
+  echo $name;
+?>
